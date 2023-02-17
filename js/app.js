6945dfcd85a5255a96adc091b607d1ec79b5f1da
AhaD <abuahad35188@gmail.com>
2023-02-18 00:47:12 +0600

    fourth card complet

--- js/app.js
+++ js/app.js
@@ -7,12 +7,13 @@ document.getElementById("first-btn").addEventListener("click",function(){
    const numb1 =  numberInput('num1');
    const numb2 =  numberInput('num10');
    const ruselt = 0.5 *(numb1 * numb2);
+   const ruselts = (ruselt).toFixed(2);
    
   const tr = document.createElement("tr")
    tr.innerHTML = `
           <td>${1}</td>
           <td>Triangle</td>
-          <td>${ruselt} cm<sup>2</sup></td>
+          <td>${ruselts} cm<sup>2</sup></td>
           <td>btn</td>
    `;
    document.getElementById("tbody-r").appendChild(tr);
@@ -24,11 +25,13 @@ document.getElementById('second-btn').addEventListener('click',function(){
      const numb2 =  numberInput('num2');
      const numb20 =  numberInput('num20');
      const ruselt = numb2* numb20;
+     const ruselts = (ruselt).toFixed(2);
+     
      const tr = document.createElement("tr")
           tr.innerHTML = `
           <td>${2}</td>
           <td>Rectangle</td>
-          <td>${ruselt} cm<sup>2</sup></td>
+          <td>${ruselts} cm<sup>2</sup></td>
           <td>btn</td>
    `;
    document.getElementById("tbody-r").appendChild(tr)
@@ -39,18 +42,34 @@ document.getElementById('thired-card').addEventListener("click",function(){
      const n3 = getinnText('num3');
      const n30 = getinnText('num30');
      const ruselt = n3 * n30 ;
+     const ruselts = (ruselt).toFixed(2);
 
      const tr = document.createElement("tr")
           tr.innerHTML = `
-          <td>${1}</td>
+          <td>${3}</td>
           <td>Triangle</td>
-          <td>${ruselt} cm<sup>2</sup></td>
+          <td>${ruselts} cm<sup>2</sup></td>
           <td>btn</td>
    `;
    document.getElementById("tbody-r").appendChild(tr);
 });
 
 // fourth card
+document.getElementById("fourth-card").addEventListener("click",function(){
+     const num4 = getinnText('num4');
+     const num40 = getinnText('num40');
+     const ruselt = 0.5 * num4 * num40;
+     const ruselts = (ruselt).toFixed(2);
+
+     const tr = document.createElement("tr")
+          tr.innerHTML = `
+          <td>${4}</td>
+          <td>Triangle</td>
+          <td>${ruselts} cm<sup>2</sup></td>
+          <td>btn</td>
+   `;
+   document.getElementById("tbody-r").appendChild(tr);
+})
 
 
 
